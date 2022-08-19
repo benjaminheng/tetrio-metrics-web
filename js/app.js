f4d5d24c750100ef6783a629cbdc480ead271b3b
Benjamin Heng <benjamin.heng@thecarousell.com>
2022-08-19 19:02:58 +0800

    Add chart for 40l performance over games played

--- js/app.js
+++ js/app.js
@@ -43,8 +43,7 @@ class Renderer {
     });
   }
 
-  render40LTimeChart() {
-    const { spline } = uPlot.paths;
+  render40LOverTime() {
     const xAxis = [];
     const yAxis = [];
     for (const v of this.data) {
@@ -53,36 +52,83 @@ class Renderer {
     }
     const data = [xAxis, yAxis]
     let opts = {
-      title: "40L time",
-      id: "40l-time-chart",
+      title: "40L performance over time",
+      id: "40l-over-time-chart",
       width: 800,
-      height: 400,
+      height: 300,
       series: [
-        {},
+        {
+          label: "Timestamp",
+        },
         {
           // initial toggled state (optional)
           show: true,
           // in-legend display
-          label: "time",
+          label: "Time",
           value: (self, rawValue) => rawValue + "s",
 
           // series style
           stroke: "red",
           width: 1,
-          // paths: spline(),
+          drawStyle: null,
+        }
+      ],
+    };
+    let uplot = new uPlot(opts, data, document.getElementById("40l-over-time-container"));
+  }
+
+  render40LOverGamesPlayed() {
+    const { spline } = uPlot.paths;
+    const xAxis = [];
+    const yAxis = [];
+    let count = 1;
+    for (const v of this.data) {
+      xAxis.push(count++);
+      yAxis.push(v.time);
+    }
+    const data = [xAxis, yAxis]
+    let opts = {
+      title: "40L performance over games played",
+      id: "40l-over-games-played-chart",
+      width: 800,
+      height: 300,
+      scales: {
+        x: {
+          time: false,
+        },
+        y: {
+          auto: true,
+        }
+      },
+      series: [
+        {
+          label: "Game number",
+        },
+        {
+          // initial toggled state (optional)
+          show: true,
+          // in-legend display
+          label: "Time",
+          value: (self, rawValue) => rawValue + "s",
+
+          // series style
+          stroke: "red",
+          width: 1,
+          drawStyle: null,
+          paths: spline(),
         }
       ],
     };
 
-    let uplot = new uPlot(opts, data, document.getElementById("40l-time-container"));
+    let uplot = new uPlot(opts, data, document.getElementById("40l-over-games-played-container"));
   }
-
 }
 
 async function main() {
   const renderer = new Renderer();
   await renderer.initData();
-  renderer.render40LTimeChart();
+  renderer.render40LOverTime();
+  renderer.render40LOverGamesPlayed();
 }
 
 onDocumentReady(main)
